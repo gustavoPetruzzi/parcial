25dce6605d1b33b3239a081c235ffc0f79d89f7f
mavocado3 <gustavopetruzzi19@gmail.com>
2018-05-07 12:46:59 -0300

    Paso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { miHttp } from './mihttp.service';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [miHttp],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/mihttp.service.ts
+++ src/app/mihttp.service.ts
@@ -1,8 +1,69 @@
+import { log } from 'util';
 import { Injectable } from '@angular/core';
 
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+
 @Injectable()
-export class MihttpService {
+export class MiHttpService {
 
-  constructor() { }
+  constructor( public http: Http ) { }
+  api = "localhost/parcial/index.php/"
+  headers = new Headers();
+  headers.append("Content-type", "application/json")
 
+  public httpGetP ( url: string)
+  {
+    return this.http
+    .get( url )
+    .toPromise()
+    .then( this.extractData )
+    .catch( this.handleError );
+  }
+
+  public httpPostP( url: string, objeto: any )
+  {
+    return this.http
+    .get( url )
+    .subscribe( data => {
+      console.log( data );
+      return data;
+    });
+  }
+
+   public httpPostData( url: string , data:any ){
+    
+    let options = new RequestOptions({ headers: this.headers });
+    return this.http
+      .post( this.api + url, data, options)
+      .toPromise()
+      .then( this.extractData )
+      .catch( this.handleError )
+  }
+
+
+
+
+
+  public httpGetO ( url: string): Observable<Response>
+  {
+    return this.http.get( url )
+      .map( ( res: Response ) => res.json())
+      .catch( ( err: any ) => Observable.throw(err.json().error || 'Server error'));
+  }
+
+
+  private extractData ( res: Response )
+  {
+    return res.json() || {};
+  }
+
+  private handleError ( error: Response | any )
+  {
+    return error;
+  }
 }
